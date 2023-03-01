5f4e67eb74e966a27795c9a7c6f4e66a813c832e
Dmither <mr.d.graumann@gmail.com>
2023-03-01 10:31:46 +0200

    update

--- main.py
+++ main.py
@@ -1,33 +1,50 @@
 import pygame
 
-# init game, set screen resolution and game name
-pygame.init()
-screen = pygame.display.set_mode((640, 480))  # flags=pygame.NOFRAME
-pygame.display.set_caption("PyGame lessons")
+clock = pygame.time.Clock()
 
-# Add and set game icon
+pygame.init()
+screen = pygame.display.set_mode((160, 120)) 
+pygame.display.set_caption("PyGame lessons")
 icon = pygame.image.load("./data/img/game-icon.png")
 pygame.display.set_icon(icon)
 
+bgcolor = (252, 223, 205)
 
-# variables:
-square = pygame.Surface((50, 100))
-square.fill((255, 227, 92))
+walk_right = [
+    pygame.image.load("./data/img/player/tile_0045.png"),
+    pygame.image.load("./data/img/player/tile_0046.png")
+]
 
-myFont = pygame.font.Font("./data/fonts/VT323.ttf", 30)
-text_surface = myFont.render("The first game on PYGAME", True, "Red")
+ground_tile = pygame.image.load("./data/img/tile_0004.png")
+ground = pygame.Surface((320, 16))
+ground.blit(ground_tile, (0, 0))
+ground.blit(ground_tile, (16, 0))
+ground.blit(ground_tile, (32, 0))
+ground.blit(ground_tile, (48, 0))
+ground.blit(ground_tile, (64, 0))
+ground.blit(ground_tile, (80, 0))
+ground.blit(ground_tile, (96, 0))
+ground.blit(ground_tile, (112, 0))
+ground.blit(ground_tile, (128, 0))
+ground.blit(ground_tile, (144, 0))
 
+player_anim_count = 0
+frame_count = 0
+max_frame = 20
 
-
-# set game status and main cycle
 running = True
 while running:
 
-    # screen.blit(square, (100, 50))
-    # pygame.draw.circle(square, "red", (0, 50), 30)
+    screen.fill(bgcolor)
 
-    screen.blit(text_surface, (180, 300))
-    screen.blit(icon, (250, 120))
+    screen.blit(walk_right[player_anim_count], (16, 64))
+    screen.blit(ground, (0, 80))
+
+    if frame_count % 5 == 0:
+        if player_anim_count == 0:
+            player_anim_count = 1
+        else:
+            player_anim_count = 0
 
 
 
@@ -38,3 +55,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+
+    clock.tick(max_frame)
+    frame_count += 1
